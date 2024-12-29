d3e25b00d18305c8766a9153c88420e5374d539e
Sofa Kliuchkovska <sofakliuchkovska@MacBook-Air-Sofa.local>
2024-12-29 23:24:35 +0100

    feat: getIdentityMatrix done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -397,8 +397,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (row, i) => {
+    return Array.from({ length: n }, (cell, j) => {
+      return i === j ? 1 : 0;
+    });
+  });
 }
 
 /**
