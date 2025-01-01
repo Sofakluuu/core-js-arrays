8e06e3ae8bad6aaf1a5ef874077758f217deccfa
Sofa Kliuchkovska <sofakliuchkovska@MacBook-Air-Sofa.local>
2025-01-01 21:28:23 +0100

    feat: createChunks done

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -340,8 +340,11 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize8 */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize8) {
+  return Array.from(
+    { length: Math.ceil(arr.length / chunkSize8) },
+    (_, index) => arr.slice(index * chunkSize8, (index + 1) * chunkSize8)
+  );
 }
 
 /**
